55bad628bc78f6fc4e5e574be22e588f70072b0e
Solix <81125185+DaydexB@users.noreply.github.com>
2024-04-07 23:11:57 -0500

    Update Config.lua

--- solix_bong/Config.lua
+++ solix_bong/Config.lua
@@ -3,7 +3,7 @@ Config = {}
 Config.BongProps = {'md_bong1_model', 'md_bong2_model','md_bong3_model', 'md_bong4_model','md_bong5_model', 'md_bong6_model','md_bong7_model', 'hei_heist_sh_bong_01', 'prop_bong_01', 'prop_sh_bong_01' }
 -- You can change/add/remove any of these i have all the ones from the white widow cafe and the default GTA ones 
 
-Config.PayToSmoke = true
+Config.PayToSmoke = true -- If true players will have to pay Config.PriceToSmoke to smoke the bong if false players will not have to pay
 
 Config.PriceToSmoke = 300 -- $300 for this to work Config.PayToSmoke has to be true
 
